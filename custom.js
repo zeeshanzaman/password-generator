09c77b316634a1a7c5b95e61fbd2535538e5a4b0
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-09-28 12:15:00 +0500

    function for random upper

--- custom.js
+++ custom.js
@@ -1,5 +1,12 @@
 function getRandomLower() {
-    return String.fromCharCode(97)
+    return String.fromCharCode(Math.floor(Math.random() * 26) +
+        97)
 }
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) +
+        65)
+}
+
 
 console.log(getRandomLower())
